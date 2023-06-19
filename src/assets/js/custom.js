549d8442e8d14facbc3cbb14386a7e8d700d0ade
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2023-06-19 16:51:18 +0100

    Bitcoin Trade API

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -141,15 +141,15 @@
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
       // You can unslick at a given breakpoint now by adding:
